4af563eaede7c8530cc0b6841f772ee0cfb9e690
Mikhail Brann <alunos13@yandex.com>
2023-12-06 23:30:10 +0300

    array hw aaded

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length == arr2.length &&
+        arr1.every((element, index) => element === arr2[index] && index === arr2.indexOf(element));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter(user => user.gender == gender && users.length)
+        .reduce((acc, userObj, index, arr) => {
+
+            if(index === arr.length - 1) {
+                acc += userObj.age;
+                return acc / arr.length;
+            }
+
+            return acc + userObj.age;
+        }, 0);
 }
