8d05673d6c5af4da7d1ba324056dc1bf6371cb3f
Mikhail Brann <alunos13@yandex.com>
2023-12-16 14:14:37 +0300

    added hw exception

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,61 @@
-﻿
+﻿class Triangle {
+    constructor(a, b, c) {
+        if(a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+
+    get perimeter() {
+        this._perimeter = this.a + this.b + this.c;
+        return this._perimeter;
+    }
+
+    get area() {
+        const halfPerimetr = this.perimeter / 2;
+        const calcResult = Math.sqrt(
+            halfPerimetr * (halfPerimetr - this.a) * (halfPerimetr - this.b) * (halfPerimetr - this.c)
+        ).toFixed(3);
+        this._area = parseFloat(calcResult);
+
+        return this._area;
+    }
+}
+
+function getTriangle(...params) {
+    try {
+        const triangle = new Triangle(...params);
+        return triangle;
+    } catch (error) {
+        const errorObj = {
+            perimeter: "Ошибка! Треугольник не существует",
+            area: "Ошибка! Треугольник не существует",
+        };
+
+        Object.freeze(errorObj);
+
+        return errorObj;
+    }
+}
+
+function parseCount(inputValue) {
+    const result = Number.parseFloat(inputValue);
+
+    if(Number.isNaN(result)) {
+        throw new Error('Невалидное значение');
+    }
+
+    return  result;
+}
+
+function validateCount(inputValue) {
+    try {
+        const validateValue = parseCount(inputValue);
+        return validateValue;
+    } catch (error) {
+        return error;
+    }
+}
