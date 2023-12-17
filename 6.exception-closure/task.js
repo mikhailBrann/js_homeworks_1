5e6c8fddd42dd531c3c9dcdd4a0b18a8863c1363
Mikhail Brann <alunos13@yandex.com>
2023-12-17 21:53:37 +0300

    add exception refactor

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -2,16 +2,15 @@
     constructor(a, b, c) {
         if(a + b < c || a + c < b || b + c < a) {
             throw new Error("Треугольник с такими сторонами не существует");
-        } else {
-            this.a = a;
-            this.b = b;
-            this.c = c;
         }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
     get perimeter() {
-        this._perimeter = this.a + this.b + this.c;
-        return this._perimeter;
+        return this.a + this.b + this.c;
     }
 
     get area() {
@@ -19,9 +18,8 @@
         const calcResult = Math.sqrt(
             halfPerimetr * (halfPerimetr - this.a) * (halfPerimetr - this.b) * (halfPerimetr - this.c)
         ).toFixed(3);
-        this._area = parseFloat(calcResult);
 
-        return this._area;
+        return parseFloat(calcResult);
     }
 }
 
